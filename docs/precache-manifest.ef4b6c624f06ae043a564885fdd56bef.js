4d2d154b729381ef6ad6a3e88caf000b6db75fae
gonzalez-marian <gonzalezmarian170@gmail.com>
2020-02-11 11:38:02 +0100

    add docs

---
+++ docs/precache-manifest.ef4b6c624f06ae043a564885fdd56bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d060210453c681901ee2a346eee5bc59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82146da25236dafbe0a0",
+    "url": "./static/css/main.0ea41fdb.chunk.css"
+  },
+  {
+    "revision": "4d61fed769da31be3b79",
+    "url": "./static/js/2.af4a3abb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.af4a3abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82146da25236dafbe0a0",
+    "url": "./static/js/main.7df6e3cb.chunk.js"
+  },
+  {
+    "revision": "e3a31b488e43bc053c41",
+    "url": "./static/js/runtime-main.d001bfb9.js"
+  }
+]);
